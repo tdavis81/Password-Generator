37456b98e65693c2a547a90198aecd72d391ecac
tdavis81 <35125581+tdavis81@users.noreply.github.com>
2018-06-08 22:54:22 -0400

    Add files via upload

---
+++ Password Changer/Password Changer/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(107, 88)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 30)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(9, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(276, 55)
+        Me.TextBox1.TabIndex = 1
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(28, 155)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(79, 17)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "LowerCase"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(124, 155)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(79, 17)
+        Me.CheckBox2.TabIndex = 3
+        Me.CheckBox2.Text = "UpperCase"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(220, 155)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(68, 17)
+        Me.CheckBox3.TabIndex = 4
+        Me.CheckBox3.Text = "Numbers"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(135, 199)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Pass Length"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(9, 197)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 7
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 233)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(276, 28)
+        Me.ProgressBar1.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(229, 199)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Label2"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(325, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(35, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(325, 273)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Password Changer"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Password Changer/Password Changer/Form1.vb
@@ -0,0 +1,127 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label2.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        NumericUpDown1.Text.Trim()
+        If CheckBox1.Checked = False And CheckBox2.Checked = False And CheckBox3.Checked = False Then
+            MsgBox("You Must Select A Method ", MsgBoxStyle.Critical) 'You can always change your message
+        End If
+
+        If Val(NumericUpDown1.Text) <= 3 Then 'You can change the minimum number
+            MsgBox("Please enter a number higher than three", MsgBoxStyle.Critical, )
+            Exit Sub 'You can change your mwssage
+
+        End If
+        If Val(NumericUpDown1.Text) > 78 Then 'You can change the maximum number.
+            MsgBox("number is too high max 78", MsgBoxStyle.Critical, )
+
+            Exit Sub
+
+        End If
+        TextBox1.Text = GenerateCode()
+    End Sub
+    Public Function GenerateCode()
+        Dim intRnd As Integer
+        Dim intStep As Integer = Nothing
+        Dim strname As String
+        Dim intlength As Integer
+        Dim strinputstring As String = ""
+        Dim Numbers As String = "1234567890"
+        Dim Lower As String = "abcdefghijklmnopqrstuvwxyzyz"
+        Dim Upper As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZYZ"
+        Dim intnamelength As Integer = 1
+
+
+        If CheckBox1.Checked Then strinputstring &= Lower
+        If CheckBox2.Checked Then strinputstring &= Numbers
+        If CheckBox3.Checked Then strinputstring &= Upper
+
+
+
+
+        intlength = Len(strinputstring)
+
+
+
+        Integer.TryParse(NumericUpDown1.Text, intnamelength)
+
+        Randomize()
+
+
+        strname = ""
+
+
+        For inStep = 1 To intnamelength
+
+
+            intRnd = Int(Rnd() * intlength) + 1
+
+
+            strname = strname & Mid(strinputstring, intRnd, 1)
+
+
+        Next
+
+
+        Return strname
+
+    End Function
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.TextLength < 9 Then
+            ProgressBar1.Value = 5
+
+        End If
+
+        If TextBox1.TextLength > 9 Then
+            ProgressBar1.Value = 25
+        End If
+        If TextBox1.TextLength > 19 Then
+            ProgressBar1.Value = 50
+        End If
+        If TextBox1.TextLength > 30 Then
+            ProgressBar1.Value = 75
+        End If
+        If TextBox1.TextLength > 51 Then
+            ProgressBar1.Value = 100
+        End If
+        If TextBox1.TextLength < 8 Then
+            Label2.Text = "Poor!!"
+            Label2.ForeColor = Color.White
+            Label2.BackColor = Color.Red
+            Me.BackColor = Color.Red
+            MenuStrip1.BackColor = Color.Red
+            CheckBox1.ForeColor = Color.White
+            CheckBox2.ForeColor = Color.White
+            CheckBox3.ForeColor = Color.White
+            MenuStrip1.ForeColor = Color.White
+            Label1.ForeColor = Color.White
+        End If
+        If TextBox1.TextLength > 8 Then
+            Label2.Text = "Good"
+            Label2.ForeColor = Color.White
+            Label2.BackColor = Color.Green
+            Me.BackColor = Color.Green
+            MenuStrip1.BackColor = Color.Green
+            CheckBox1.ForeColor = Color.White
+            CheckBox2.ForeColor = Color.White
+            CheckBox3.ForeColor = Color.White
+            MenuStrip1.ForeColor = Color.White
+            Label1.ForeColor = Color.White
+        End If
+        If ProgressBar1.Value > 20 Then
+            Label2.Text = "Excellent"
+            Label2.ForeColor = Color.White
+            Label2.BackColor = Color.Green
+        End If
+        If ProgressBar1.Value > 50 Then
+            Label2.Text = "Outstanding"
+            Label2.ForeColor = Color.White
+            Label2.BackColor = Color.Green
+        End If
+    End Sub
+
+End Class
